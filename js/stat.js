c1a088f26bf491655e1ce1b676a5c66c5af9f707
da_ladno_4e_tam <mixa.awesome@gmail.com>
2020-09-22 19:06:40 +0300

    Разбил код функции renderStatistics на несколько функций

--- js/stat.js
+++ js/stat.js
@@ -4,17 +4,24 @@ const CLOUD_WIDTH = 420;
 const CLOUD_HEIGHT = 270;
 const CLOUD_X = 100;
 const CLOUD_Y = 10;
-const MAX_COLUMN_HEIGHT = 150;
-const COLUMN_WIDTH = 40;
-const COLUMN_CAP = 50;
-const COLUMN_VERTICAL_PADDING = 8;
 const CLOUD_VERTICAL_PADDING = 14;
+const CLOUD_GAP = 10;
+const COLUMN_WIDTH = 40;
+const COLUMN_MAX_HEIGHT = 150;
+const COLUMN_GAP = 50;
+const COLUMN_VERTICAL_PADDING = 8;
+const TEXT_LEFT_PADDING = 36;
 const FONT_SIZE = 16;
 const LINE_HEIGHT = 18;
 
 function renderRect(ctx, x, y, width, height, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
+}
+
+function renderCloud(ctx) {
+  renderRect(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');
+  renderRect(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#fff');
 }
 
 function renderText(ctx, text, x, y) {
@@ -24,23 +31,47 @@ function renderText(ctx, text, x, y) {
   ctx.fillText(text, x, y);
 }
 
+function renderResultText(ctx) {
+  renderText(ctx, 'Ура вы победили!', CLOUD_X + TEXT_LEFT_PADDING, CLOUD_Y + CLOUD_VERTICAL_PADDING);
+  renderText(ctx, 'Список результатов:', CLOUD_X + TEXT_LEFT_PADDING, CLOUD_Y + CLOUD_VERTICAL_PADDING + LINE_HEIGHT);
+}
+
+function getMaxElement(arr) {
+  let maxElement = Math.max(...arr);
+  return maxElement;
+}
+
+function getColumnHeight(playerTime, times) {
+  const maxTime = getMaxElement(times);
+  const columnHeight = COLUMN_MAX_HEIGHT * playerTime / maxTime;
+  return columnHeight;
+}
+
+function getRandomSaturation() {
+  let saturation = Math.floor(Math.random() * 101);
+  return saturation;
+}
+
+function getColumnColor(player) {
+  let columnColor = 'rgba(255, 0, 0, 1)';
+  if (player !== 'Вы') {
+    columnColor = `hsl(240, ${getRandomSaturation()}%, 50%)`;
+  }
+  return columnColor;
+}
+
 window.renderStatistics = function (ctx, players, times) {
-  const maxTime = Math.max(...times);
-  const PADDING_LEFT = (CLOUD_WIDTH - players.length * COLUMN_WIDTH - (players.length - 1) * COLUMN_CAP) / 2;
+  const sidePadding = (CLOUD_WIDTH - players.length * COLUMN_WIDTH - (players.length - 1) * COLUMN_GAP) / 2;
 
-  renderRect(ctx, CLOUD_X + 10, CLOUD_Y + 10, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');
-  renderRect(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#fff');
-
-  renderText(ctx, 'Ура вы победили!', CLOUD_X + PADDING_LEFT, CLOUD_Y + CLOUD_VERTICAL_PADDING);
-  renderText(ctx, 'Список результатов:', CLOUD_X + PADDING_LEFT, CLOUD_Y + CLOUD_VERTICAL_PADDING + LINE_HEIGHT);
+  renderCloud(ctx);
+  renderResultText(ctx);
 
   for (let i = 0; i < players.length; i++) {
-    renderText(ctx, players[i], CLOUD_X + PADDING_LEFT + (COLUMN_WIDTH + COLUMN_CAP) * i, CLOUD_Y + CLOUD_HEIGHT - (CLOUD_VERTICAL_PADDING + FONT_SIZE));
-    if (players[i] === 'Вы') {
-      renderRect(ctx, CLOUD_X + PADDING_LEFT + (COLUMN_WIDTH + COLUMN_CAP) * i, CLOUD_Y + CLOUD_HEIGHT - (CLOUD_VERTICAL_PADDING + FONT_SIZE + MAX_COLUMN_HEIGHT * times[i] / maxTime + COLUMN_VERTICAL_PADDING), COLUMN_WIDTH, MAX_COLUMN_HEIGHT * times[i] / maxTime, 'rgba(255, 0, 0, 1)');
-    } else {
-      renderRect(ctx, CLOUD_X + PADDING_LEFT + (COLUMN_WIDTH + COLUMN_CAP) * i, CLOUD_Y + CLOUD_HEIGHT - (CLOUD_VERTICAL_PADDING + FONT_SIZE + MAX_COLUMN_HEIGHT * times[i] / maxTime + COLUMN_VERTICAL_PADDING), COLUMN_WIDTH, MAX_COLUMN_HEIGHT * times[i] / maxTime, `hsl(240, ${Math.floor(Math.random() * 101)}%, 50%)`);
-    }
-    renderText(ctx, Math.round(times[i]), CLOUD_X + PADDING_LEFT + (COLUMN_WIDTH + COLUMN_CAP) * i, CLOUD_Y + CLOUD_HEIGHT - (CLOUD_VERTICAL_PADDING + 2 * FONT_SIZE + MAX_COLUMN_HEIGHT * times[i] / maxTime + 2 * COLUMN_VERTICAL_PADDING));
+    const columnX = CLOUD_X + sidePadding + (COLUMN_WIDTH + COLUMN_GAP) * i;
+    const columnY = CLOUD_Y + CLOUD_HEIGHT - (CLOUD_VERTICAL_PADDING + FONT_SIZE + getColumnHeight(times[i], times) + COLUMN_VERTICAL_PADDING);
+
+    renderText(ctx, players[i], columnX, CLOUD_Y + CLOUD_HEIGHT - (CLOUD_VERTICAL_PADDING + FONT_SIZE));
+    renderRect(ctx, columnX, columnY, COLUMN_WIDTH, getColumnHeight(times[i], times), getColumnColor(players[i]));
+    renderText(ctx, Math.round(times[i]), columnX, columnY - (FONT_SIZE + COLUMN_VERTICAL_PADDING));
   }
 };
