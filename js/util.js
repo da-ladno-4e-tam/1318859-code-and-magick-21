efd98817599d745a83ae147337d518f8876e5249
da_ladno_4e_tam <mixa.awesome@gmail.com>
2020-10-06 20:06:50 +0300

    Объединил мелкие модули

--- js/util.js
+++ js/util.js
@@ -4,6 +4,25 @@
   const KEY_ENTER = 'Enter';
   const KEY_ESCAPE = 'Escape';
 
+  window.getRandomElement = function (arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+
+  window.getContent = function (render, arr, container) {
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < arr.length; i++) {
+      fragment.appendChild(render(arr[i]));
+    }
+    container.appendChild(fragment);
+  };
+
+  window.getElementsList = function (length, list, getElement) {
+    for (let i = 0; i < length; i++) {
+      list.push(getElement());
+    }
+    return list;
+  };
+
   window.util = {
     isEnterEvent: function (evt, action) {
       if (evt.key === KEY_ENTER) {
