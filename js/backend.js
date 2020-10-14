e30084fa0c05ee6cb25e6269b4771760ae6a988d
da_ladno_4e_tam <mixa.awesome@gmail.com>
2020-10-15 00:23:06 +0300

    Исправил ошибки в backend.js

--- js/backend.js
+++ js/backend.js
@@ -1,66 +1,50 @@
 'use strict';
 
 (function () {
+  const TIMEOUT = 3000;
   const StatusCode = {
     OK: 200
   };
-  const TIMEOUT = 3000;
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.timeout = TIMEOUT;
+
+  function catchResponseErrors(onSuccess, onError, flag) {
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText, flag);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения', flag);
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс', flag);
+    });
+  }
+
+  function load(onLoad, onError) {
+    const URL = 'https://21.javascript.pages.academy/code-and-magick/data';
+
+    catchResponseErrors(onLoad, onError);
+    xhr.open('GET', URL);
+    xhr.send();
+  }
+
+  function save(data, onSave, onError) {
+    const URL = 'https://21.javascript.pages.academy/code-and-magick';
+
+    catchResponseErrors(onSave, onError, 'save');
+    xhr.open('POST', URL);
+    xhr.send(data);
+  }
 
   window.backend = {
-    load: function (onLoad, onError) {
-      const URL = 'https://21.javascript.pages.academy/code-and-magick/data';
-      const xhr = new XMLHttpRequest();
-
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === StatusCode.OK) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = TIMEOUT;
-
-      xhr.open('GET', URL);
-      xhr.send();
-    },
-
-    save: function (data, onLoad, onError) {
-      const URL = 'https://21.javascript.pages.academy/code-and-magick';
-      const xhr = new XMLHttpRequest();
-
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === StatusCode.OK) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText, 'save');
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения', 'save');
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс', 'save');
-      });
-
-      xhr.timeout = TIMEOUT;
-
-      xhr.open('POST', URL);
-      xhr.send(data);
-    }
+    load: load,
+    save: save
   };
 })();
